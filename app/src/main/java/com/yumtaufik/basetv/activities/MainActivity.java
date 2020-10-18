c1f662c7fcc910ca5cf552f76545fd6d7f553445
yumtaufikhidayat <yumtaufik1997@gmail.com>
2020-10-18 18:12:09 +0700

    Adding endless pagination into main activity.

--- app/src/main/java/com/yumtaufik/basetv/activities/MainActivity.java
+++ app/src/main/java/com/yumtaufik/basetv/activities/MainActivity.java
@@ -2,9 +2,11 @@ package com.yumtaufik.basetv.activities;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.yumtaufik.basetv.R;
 import com.yumtaufik.basetv.adapters.TVShowsAdapter;
@@ -19,8 +21,11 @@ public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding activityMainBinding;
     private MostPopularTVShowsViewModel mostPopularTVShowsViewModel;
-    private final List<TVShowsItems> tvShowsItemsList = new ArrayList<>();
     private TVShowsAdapter tvShowsAdapter;
+
+    private final List<TVShowsItems> tvShowsItemsList = new ArrayList<>();
+    private int currentPage = 1;
+    private int totalAvailablePages = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,18 +42,40 @@ public class MainActivity extends AppCompatActivity {
         mostPopularTVShowsViewModel = new ViewModelProvider(this).get(MostPopularTVShowsViewModel.class);
         tvShowsAdapter = new TVShowsAdapter(tvShowsItemsList);
         activityMainBinding.rvTvShows.setAdapter(tvShowsAdapter);
-    }
-
-    private void getMostPopularTVShows() {
-        activityMainBinding.setIsLoading(true);
-        mostPopularTVShowsViewModel.getMostPopularTVShows(0).observe(this, mostPopularTVShowsResponse -> {
-            activityMainBinding.setIsLoading(false);
-            if (mostPopularTVShowsResponse != null) {
-                if (mostPopularTVShowsResponse.getTv_shows() != null) {
-                    tvShowsItemsList.addAll(mostPopularTVShowsResponse.getTv_shows());
-                    tvShowsAdapter.notifyDataSetChanged();
+        activityMainBinding.rvTvShows.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                if (!activityMainBinding.rvTvShows.canScrollVertically(1)) {
+                    if (currentPage <= totalAvailablePages) {
+                        currentPage += 1;
+                        getMostPopularTVShows();
+                    }
                 }
             }
         });
+    }
+
+    private void getMostPopularTVShows() {
+        toggleLoading();
+        mostPopularTVShowsViewModel.getMostPopularTVShows(currentPage).observe(this, mostPopularTVShowsResponse -> {
+            toggleLoading();
+            if (mostPopularTVShowsResponse != null) {
+                totalAvailablePages = mostPopularTVShowsResponse.getPages();
+                if (mostPopularTVShowsResponse.getTv_shows() != null) {
+                    int oldCount = tvShowsItemsList.size();
+                    tvShowsItemsList.addAll(mostPopularTVShowsResponse.getTv_shows());
+                    tvShowsAdapter.notifyItemRangeInserted(oldCount, tvShowsItemsList.size());
+                }
+            }
+        });
+    }
+
+    private void toggleLoading() {
+        if (currentPage == 1) {
+            activityMainBinding.setIsLoading(activityMainBinding.getIsLoading() == null || !activityMainBinding.getIsLoading());
+        } else {
+            activityMainBinding.setIsLoadingMore(activityMainBinding.getIsLoadingMore() == null || !activityMainBinding.getIsLoadingMore());
+        }
     }
 }
