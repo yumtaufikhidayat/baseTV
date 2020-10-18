13545d5392db40ffc102407613a25dbbf60e3e30
yumtaufikhidayat <yumtaufik1997@gmail.com>
2020-10-18 12:11:08 +0700

    Fixing fonts name.

--- app/src/main/java/com/yumtaufik/basetv/activity/MainActivity.java
+++ app/src/main/java/com/yumtaufik/basetv/activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.yumtaufik.basetv;
+package com.yumtaufik.basetv.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.yumtaufik.basetv.R;
 
 public class MainActivity extends AppCompatActivity {
 
